00f946002ea94c4f0120e75fde3a5567ee770a58
Edina Balog <Edina Balog>
2017-02-05 16:38:29 +0200

    db path added

--- DBMS/Utilities/UICommand.cs
+++ DBMS/Utilities/UICommand.cs
@@ -28,19 +28,21 @@ namespace Utilities
     {
         public CommandType Command { get; set; }
         public EntityType Entity { get; set; }
+        public string Path { get; set; }
         public List<string> TableNames { get; set; }
         public List<TableColumn> Columns { get; set; }
         public string ErrorMessage { get; set; }
         public bool Success { get; set; }
         public override string ToString()
         {
-            string str = string.Format("Command: {0}\nEntity: {1}\nSuccess: {2}\nTables: {3}\nColumns: {4}\nError message: {5}\n", 
+            string str = string.Format("Command: {0}\nEntity: {1}\nSuccess: {2}\nTables: {3}\nColumns: {4}\nError message: {5}\nDatabase path: {6}\n", 
                 Command.ToString(),
                 Entity.ToString(),
                 Success,
                 string.Join(", ", TableNames),
                 string.Join(", ", Columns.Select(r => string.Format("Name: {0} | Type: {1} | Is PK: {2} | Is Unique: {3} | Is Null: {4}", r.Name, r.DataType.ToString(), r.IsPrimaryKey, r.IsUnique, r.IsNull))),
-                ErrorMessage
+                ErrorMessage,
+                Path
                 );            
 
             return str;
