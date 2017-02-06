8172797c9361494564fe5e2f2ed9e92c31b864ed
Edina Balog <Edina Balog>
2017-02-06 23:05:45 +0200

    query designer - the full bullshit...

--- DBMS/WindowQueryDesigner.xaml.cs
+++ DBMS/WindowQueryDesigner.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DBMS.Models.DBStructure;
+using DBMS.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +21,9 @@ namespace DBMS
     /// </summary>
     public partial class WindowQueryDesigner : Window
     {
+        List<DBMSDatabase> databases;
+        List<Models.DBStructure.Table> tables;
+            
         public WindowQueryDesigner()
         {
             InitializeComponent();
@@ -27,6 +32,65 @@ namespace DBMS
         private void btnAddTable_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void comboBoxTables_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string dbName = comboBoxDBs.SelectedItem.ToString();
+            FillComboTables(dbName);
+        }
+        
+        public void FillComboTables(string dbName)
+        {
+            tables = new List<Models.DBStructure.Table>();
+            
+            foreach (DBMSDatabase d in databases)
+            {
+                if (d.DatabaseName == dbName)
+                {
+                    tables = d.Tables;
+                    break;
+                }
+            }
+
+            comboBoxTables.ItemsSource = tables.Select(r => r.TableName).ToList();
+        }
+
+        private void btnAddTable_Click_1(object sender, RoutedEventArgs e)
+        {
+            string dbName = comboBoxDBs.SelectedItem.ToString();
+            string tableName = comboBoxTables.SelectedItem.ToString();
+            List<Models.DBStructure.TableColumn> columns = new List<Models.DBStructure.TableColumn>();
+
+            foreach (var d in tables)
+            {
+                if (d.TableName == tableName)
+                {
+                    columns = d.Columns;
+                    break;
+                }
+            }
+
+            textBlockTables.Text += "\nTable name:\n" + tableName + "\nColumn names:\n";
+
+            foreach (var item in columns)
+            {
+                textBlockTables.Text += item.Name + "\n";
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DBMSResult res = SchemaSerializer.LoadDatabases();
+            databases = res.Data as List<DBMSDatabase>;
+            List<string> dbNames = new List<string>();
+
+            foreach (DBMSDatabase d in databases)
+            {
+                dbNames.Add(d.DatabaseName);
+            }
+
+            this.comboBoxDBs.ItemsSource = dbNames;
         }
     }
 }
